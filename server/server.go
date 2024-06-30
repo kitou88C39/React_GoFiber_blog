681d6d451efa1a8757d58921acf26645deb5112f
kitoumasaomi <kanfutrooper@gmail.com>
2024-06-30 14:11:18 +0900

    server.goを作成する

--- server/server.go
+++ server/server.go
@@ -5,7 +5,9 @@ import "github.com/gofiber/fiber/v2"
 func main(){
 	app := fiber.New()
 	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Hello World.")
+
+
+		return c.JSON(fiber.Map{"message": "Welcome to may first web Application"})
 	})
 	app.Listen(":8000")
 }
