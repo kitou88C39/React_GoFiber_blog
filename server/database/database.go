060f9d9e3e0e708ba1fd391814988667f859fa0e
kitoumasaomi <kanfutrooper@gmail.com>
2024-07-01 20:39:08 +0900

    serverにdatabase/database.goを作成する

--- server/database/database.go
+++ server/database/database.go
@@ -1,1 +1,19 @@
 package database
+
+import (
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+)
+
+
+var DBConn *gorm.DB
+
+func main() {
+	
+	dsn := "root:neeraj@tcp(127.0.0.1:3306)/fiber_blog?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{
+		Logger: logger.Defa.Default.LogMode(logger.Error),
+
+	})
+  }
