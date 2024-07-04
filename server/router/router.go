ce97e0036b250f5b47b86babf8ad76f133488e99
kitoumasaomi <kanfutrooper@gmail.com>
2024-07-05 08:53:30 +0900

    serverにrouter/router.goを作成する

--- server/router/router.go
+++ server/router/router.go
@@ -1,5 +1,11 @@
 package router
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/neerajbg/blog/controller"
+)
 
-func setupRotes (app *fiber.Ctx) error {}
+func setupRotes (app *fiber.Ctx) error {
+	app.Get("/", controller.BlogList)
+	app.Post("/", controller.BlogCreate())
+}
