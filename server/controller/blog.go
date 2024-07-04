1ac71709a97f80914e7fe51aa1d627be11651944
kitoumasaomi <kanfutrooper@gmail.com>
2024-07-04 21:10:22 +0900

    serverにcontroller/blog.goを作成する

--- server/controller/blog.go
+++ server/controller/blog.go
@@ -2,4 +2,14 @@ package controller
 
 import "github.com/gofiber/fiber/v2"
 
-func BlogList(c *fiber.Ctx) error{}
+func BlogList(c *fiber.Ctx) error{
+	c.Status(200)
+}
+
+func BlogCreate(c *fiber.Ctx) error{
+	
+}
+
+func BlogUpdate(c *fiber.Ctx) error{
+	
+}
