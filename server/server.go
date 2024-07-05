9852b58e5183fcf10135cc0ed72c020445732d1d
kitoumasaomi <kanfutrooper@gmail.com>
2024-07-05 20:17:24 +0900

    serverにcontroller/blog.goを作成する

--- server/server.go
+++ server/server.go
@@ -1,13 +1,22 @@
 package main
 
 import (
+	"log"
+
 	"github.com/gofiber/fiber/v2"
+	"github.com/joho/godotenv"
 	"github.com/neerajbg/blog/database"
 	"github.com/neerajbg/blog/router"
 	"gorm.io/gorm/logger"
 )
 
 func init(){
+
+	if err := godotenv.Load(".env"); err != nil{
+		log.Fatal("Error in loading .env file.")
+	}
+
+
 	database.ConnectDB()
 }
 func main(){
