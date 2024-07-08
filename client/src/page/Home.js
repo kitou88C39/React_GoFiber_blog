35360a325e0f394ea5afffeb4e681d246707e307
kitoumasaomi <kanfutrooper@gmail.com>
2024-07-08 21:05:59 +0900

    srcにpageフォルダとHome.jsを作成する

--- client/src/page/Home.js
+++ client/src/page/Home.js
@@ -1,7 +1,24 @@
 import React from 'react';
 
 const Home = () => {
-  return <div>Home</div>;
-};
+  return
+  const fetchData = async () => {
+    try {
+      const apiUrl = process.env.REACT_APP_API_ROOT;
+      const response = await axios.get(apiUrl);
+
+      if (response.status === 200) {
+        if (response?.data.statusText === 'OK') {
+          setApiData(response?.data?.blog_records);
+        }
+      }
+    } catch (error) {
+      console.log(error.response);
+    }
+  };
+  fetchData();
+  return () => {};
+  }, []);
+
 
 export default Home;
